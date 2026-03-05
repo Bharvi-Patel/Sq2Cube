d859aad6569b657f69af65f905f3d90d739b34fd
bhavya-2026 <bhavya042025@gmail.com>
2026-03-05 19:22:56 +0530

    Update resize.py

--- sq2cube_AI/src/preprocessing/resize.py
+++ sq2cube_AI/src/preprocessing/resize.py
@@ -1,1 +1,8 @@
 # resize code
+import cv2
+
+def resize_image(image, size=(512, 512)):
+    """Resizes the image to the required dimensions for the 3D model."""
+    # Use INTER_AREA for shrinking, INTER_CUBIC for zooming
+    resized = cv2.resize(image, size, interpolation=cv2.INTER_AREA)
+    return resized
