c704f803d05671ce38b90c52b05ac0d5687d318b
bhavya-2026 <bhavya042025@gmail.com>
2026-03-05 19:18:10 +0530

    Update background_removal.py

--- sq2cube_AI/src/preprocessing/background_removal.py
+++ sq2cube_AI/src/preprocessing/background_removal.py
@@ -1,1 +1,13 @@
 #background_removal_code
+import rembg
+import cv2
+import numpy as np
+
+def remove_bg(input_path):
+    with open(input_path, 'rb') as f:
+        input_data = f.read()
+    subject = rembg.remove(input_data)
+    # Convert back to a format OpenCV can use
+    nparr = np.frombuffer(subject, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+    return img
